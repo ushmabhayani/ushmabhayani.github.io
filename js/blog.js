066f1ca8ede5e1de120f0c8ee128bf1015888629
techsevin <ushma.bhayani12@gmail.com>
2020-07-04 17:18:51 +0530

    js included

---
+++ js/blog.js
@@ -0,0 +1,32 @@
+function newblog(){
+    var bloghead= document.querySelector(".heading").value;
+    var publishername= document.querySelector(".publisher").value;
+    var blog = document.querySelector(".content").value;
+    var headerr = document.querySelector("#headerr");
+    var nameerr = document.querySelector("#nameerr");
+    var contenteerr = document.querySelector("#contenteerr");
+    var nameregex = /^[a-zA-Z]{3,15}$/;
+
+    if(bloghead<5 || bloghead == "" || bloghead == null){
+        document.getElementById("headerr").innerHTML = "Enter A Valid Heading";
+        return false;
+    } if(publishername < 3 || publishername =="" || publishername== null){
+        document.getElementById("nameerr").innerHTML = "Enter A Valid Name";
+        return false;
+    } if(blog =="" || blog== null){
+        document.getElementById("nameerr").innerHTML = "Enter Blog Content";
+        return false;
+    }
+
+    document.getElementById("bt").innerHTML = bloghead;
+    document.getElementById("pn").innerHTML = publishername;
+    document.getElementById("pc").innerHTML = blog;
+
+    alert("Blog Successfully Printed");
+    return true;
+
+    
+
+}
+
+
